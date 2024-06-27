32949f06138ae70e17b9cbf5944547f1de3a76d5
Kacper Duda <kapieksperimental@gmail.com>
2024-06-27 12:12:36 +0200

    Create adminpanel.php

---
+++ files/adminpanel.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/icon.png" type="image/x-icon" />
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="googlebot" content="noindex, nofollow">
+    <title>"Lipuś" - Panel Administracyjny</title>
+</head>
+<body>
+    
+
+
+</body>
+</html>
